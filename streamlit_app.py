b1f2a0f0065aa0d3cb878b0050434e60ee04257b
WinstonWare <123022817+WinstonWare@users.noreply.github.com>
2023-01-23 08:53:16 -0500

    Update streamlit_app.py
    emojies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 Blueberry and Oatmeal')
-streamlit.text('Kale, spinach, rocket smoothy')
-streamlit.text('Hard-Boiled free-range Egg')
+streamlit.header('🥑 Breakfast Menu')
+streamlit.text('🍝 Omega 3 Blueberry and Oatmeal')
+streamlit.text('🥬 Kale, spinach, rocket smoothy')
+streamlit.text('🥚 Hard-Boiled free-range Egg')
