f27e6f84452c9e4bf7a6e5e980cdd311ed6ed672
WinstonWare <123022817+WinstonWare@users.noreply.github.com>
2023-01-23 08:54:00 -0500

    Update streamlit_app.py
    Build own smoothy

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header('🥑 Breakfast Menu')
 streamlit.text('🍝 Omega 3 Blueberry and Oatmeal')
 streamlit.text('🥬 Kale, spinach, rocket smoothy')
 streamlit.text('🥚 Hard-Boiled free-range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
